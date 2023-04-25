4321561de6619adc8ff03483dcb69cf873473232
SowmiyaRam24 <sowmiramamoorthy@gmail.com>
2023-04-25 11:35:16 +0530

    3rd commite

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Expence from'./components/ExpenceItems';
 
 function App() {
+  const expenses=[
+    {title:'car Insurance',amount:299,date:new Date()},
+    {title:'bus',amount:700,date:new Date()},
+    {title:'egg',amount:898,date:new Date()}
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
-      <Expence/>
+      <Expence 
+      title={expenses[0].title}
+      amount={expenses[0].amount}
+      date={expenses[0].date}>
+
+      </Expence>
+      <Expence
+       title={expenses[1].title}
+       amount={expenses[1].amount}
+       date={expenses[1].date}>
+      </Expence>
+      <Expence  title={expenses[2].title}
+      amount={expenses[2].amount}
+      date={expenses[2].date}
+      ></Expence>
     </div>
   );
 }
--- src/components/ExpenceItems.js
+++ src/components/ExpenceItems.js
@@ -1,4 +1,19 @@
-function Expence(){
-    return<h1>WELCOME vhj</h1>
+function Expence(props){
+    const month=props.date.toLocaleString('en-US',{month:'long'});
+    const Day=props.date.toLocaleString('en-US',{day:'2-digit'});;
+    const Year=props.date.getFullYear();
+     
+    return(
+        <div>
+            <div>
+                <h1>{props.title}</h1>
+            </div>
+             <div>${props.amount}</div>
+            <div>{month}</div>
+            <div>{Day}</div>
+            <div>{Year}
+            </div>
+        </div>
+    );
 }
 export default Expence;
